e87617e2daae7297c167782003cf7c05c589905b
vuongtq <dev.trinhquocvuong@gmail.com>
2024-11-05 20:34:14 +0700

    coding right sidebar

--- app.vue
+++ app.vue
@@ -4,9 +4,19 @@
       <div class="min-h-full">
         <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
           <!--Left sidebar-->
-          <div class="md:block xs:col-span-1 xl:col-span-2">
+          <div class="hidden md:block xs:col-span-1 xl:col-span-2">
             <div class="sticky top-0">
               <SidebarLeft />
+            </div>
+          </div>
+          <!--Main content-->
+          <main class="col-span-12 bg-red-500 md:col-span-8 xl:col-span-6">
+            main
+          </main>
+          <!--Right sidebar-->
+          <div class="hidden md:block md:col-span-3 xl:col-span-4">
+            <div class="sticky top-0">
+              <SidebarRight />
             </div>
           </div>
         </div>
