0532a0ac98a97eef0dc5cb78218aff321a19401f
vuongtq <dev.trinhquocvuong@gmail.com>
2024-11-06 00:44:32 +0700

    add main section & loading

--- app.vue
+++ app.vue
@@ -10,8 +10,8 @@
             </div>
           </div>
           <!--Main content-->
-          <main class="col-span-12 bg-red-500 md:col-span-8 xl:col-span-6">
-            main
+          <main class="col-span-12 md:col-span-8 xl:col-span-6">
+            <router-view />
           </main>
           <!--Right sidebar-->
           <div class="hidden md:block md:col-span-3 xl:col-span-4">
